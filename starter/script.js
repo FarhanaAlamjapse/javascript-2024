8b7610af0e13b0793e778895f8310837db87c662
Farhana Alam Japse <farhanjapz@gmail.com>
2024-09-08 12:31:56 +0600

    added

---
+++ starter/script.js
@@ -0,0 +1,9 @@
+let js = "amazing";
+if (js === "amazing") alert("js is fun");
+console.log(40 + 8 + 23 - 10);
+
+console.log("Alam");
+console.log(11);
+
+let firstName = "japse";
+console.log(firstName);
