5fb20e89c89eda34506bd6ae15193a5f85ed7cbb
Farhana Alam Japse <farhanjapz@gmail.com>
2024-09-08 13:10:09 +0600

    data-types-added

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 if (js === "amazing") alert("js is fun");
 console.log(40 + 8 + 23 - 10);
@@ -7,3 +8,28 @@ console.log(11);
 
 let firstName = "japse";
 console.log(firstName);
+console.log(firstName);
+console.log(firstName);*/
+
+//Boolean
+let javascriptIsFun = true; //can be stored in a variable also
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "japse");
+
+//string
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+//undefined:empty value
+let year;
+console.log(typeof year); //undefined
+
+year = 1991;
+console.log(typeof year); //number
+
+//null
+console.log(typeof null);
