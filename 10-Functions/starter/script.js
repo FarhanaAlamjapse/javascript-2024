a59d724280c9701f9cf189cbe1417272d1f0de07
Farhana Alam Japse <farhanjapz@gmail.com>
2024-09-19 13:20:39 +0600

    function accepting callback function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,27 @@
 'use strict';
-//how passing arg works in function
+//function accepting other function(callback) as input
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase(); //javascriptisthebest
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+//higher order function
+const transformer = function (str, fn) {
+  console.log(`original string:${str}`); //original string:javascript is the best
+  console.log(`transformed string:${fn(str)}`); //transformed string:JAVASCRIPT is the best
+  console.log(` transformed by:${fn.name}`); //function has also property besides method.transformed by:upperFirstWord
+};
+transformer('javascript is the best', upperFirstWord);
+transformer('javascript is the best', oneWord);
+
+const high5 = function () {
+  console.log('heeyyyyyyy');
+};
+document.body.addEventListener('click', high5);
+['japse', 'afif', 'sakib'].forEach(high5);
+/*/how passing arg works in function
 const flight = 'LH234';
 const japse = {
   name: 'japse',
@@ -20,7 +42,7 @@ console.log(flight); //LH234,doesn't change.
 console.log(japse); //changed to mrs cause object
 
 const namPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 100000000);
+  person.passport = Math.trunc(Math.random() * 100000000); //changes the original one
 };
 namPassport(japse);
 checkIn(flight, japse); //incorrect,2 functions manipulating the same object
@@ -47,4 +69,4 @@ const createBooking = function (
 createBooking('LH123'); //default
 createBooking('LH123', 2, 800); //default will not evaluate;
 createBooking('LH123', 3); //calculted from default
-createBooking('LH123', undefined, 100); //if u want to leave the 2nd arg
+createBooking('LH123', undefined, 100); //if u want to leave the 2nd arg*/
