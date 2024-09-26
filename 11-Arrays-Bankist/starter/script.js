cb16fbb0568ed6dff023c76169164fa82759be41
Farhana Alam Japse <farhanjapz@gmail.com>
2024-09-26 14:16:32 +0600

    ärray methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -60,10 +60,11 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
   //.textContent=0
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements; //slice used for copy otherwise original array changes
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `<div class="movements__row">
@@ -208,7 +209,131 @@ btnClose.addEventListener('click', function (e) {
   }
   inputCloseUsername.value = inputClosePin.value = '';
 });
+let sort = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sort);
+  sort = !sort; //flip
+});
 
+//array method practice
+//1
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+console.log(bankDepositSum);
+
+//2
+const numDeposits10001 = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov >= 1000).length;
+console.log(numDeposits10001); //6
+
+//using reduce
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0); //we can do anything with reduce
+console.log(numDeposits1000); //6
+
+//3
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+console.log(deposits, withdrawals);
+//4.
+//this is a nice title->This Is a Nice
+const convertTitleCase = function (title) {
+  const capitazlize = str => str[0].toUpperCase() + str.slice(1);
+  const exceptions = ['a', 'an', 'the', 'but', 'and', 'or', 'on', 'in', 'with'];
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitazlize(word)))
+    .join(' ');
+  return capitazlize(titleCase);
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+//prefixed ++ operator
+// let a = 10;
+// console.log(a++); //10 after log increment
+// console.log(a); //11
+let a = 10;
+console.log(++a); //11 before log increment
+console.log(a); //11
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+//empty arrays +fill method
+const x = new Array(7);
+console.log(x); //[empty × 7]
+console.log(x.map(() => 5)); //doesnt map still empty
+x.fill(1, 3, 5); // [empty × 3, 1, 1, empty × 2] like slice
+x.fill(1); //[1, 1, 1, 1, 1, 1, 1]
+console.log(x);
+
+arr.fill(23, 2, 6); //[1, 2, 23, 23, 23, 23, 7]
+console.log(arr);
+
+//Array.from()
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y); //[1, 1, 1, 1, 1, 1, 1]
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1); //extaclty like map
+console.log(z); //[1, 2, 3, 4, 5, 6, 7]
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('É', ''))
+  );
+  console.log(movementsUI);
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+  console.log(movementsUI2);
+});
+
+/*
+//sort
+//strings
+const owners = ['japse', 'arafat', 'poly', 'mily', 'nishat'];
+console.log(owners.sort()); //['arafat', 'japse', 'mily', 'nishat', 'poly']
+console.log(owners); ////['arafat', 'japse', 'mily', 'nishat', 'poly']..mutates
+//numbers
+console.log(movements);
+//console.log(movements.sort());// [-130, -400, -650, 1300, 200, 3000, 450, 70] doesnt work.
+
+//return <0 A,B keep order
+//return >0,B,A switch order
+
+//ascending order
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (b > a) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+//descending order
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (b > a) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
+
+/*
 //flat
 const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
 console.log(arr.flat()); //[1, 2, 3, 4, 5, 6, 7, 8] 1 level of nesting
@@ -231,7 +356,7 @@ const overalBalance = accounts
 console.log(overalBalance);
 //flatmap comined flat and map
 const overalBalance2 = accounts
-  .flatMap(acc => acc.movements)
+  .flatMap(acc => acc.movements) //only goes 1 level deep.if it nested then use flat
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2);
 
