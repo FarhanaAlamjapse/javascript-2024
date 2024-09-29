0040003d453a9fac0626bd07d68337b7e6913c79
Farhana Alam Japse <farhanjapz@gmail.com>
2024-09-30 00:01:49 +0600

    prototype

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -2,12 +2,13 @@
 //function constructor
 //arrow func will not work as function constructor.as its dont have own this keyword
 const Person = function (firstName, birthYear) {
+  //instance properties
   this.firstName = firstName;
   this.birthYear = birthYear;
   //never use this..
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
 };
 const jonas = new Person('jonas', 1991);
 console.log(jonas); //Person {firstName: 'jonas', birthYear: 1991, calcAge: ƒ}
@@ -21,4 +22,23 @@ const matilda = new Person('matilda', 2017);
 const jack = new Person('jack', 1975);
 console.log(matilda, jack); //Person {firstName: 'matilda', birthYear: 2017, calcAge: ƒ} Person {firstName: 'jack', birthYear: 1975, calcAge: ƒ}
 
-console.log(jonas instanceof Person);
+console.log(jonas instanceof Person); //true
+
+//prototype
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+jonas.calcAge(); //46 we get result
+matilda.calcAge(); //20
+console.log(jonas.__proto__); //{calcAge: ƒ} prototype of jonas,we see functn so jonas is able to use this
+console.log(jonas.__proto__ === Person.prototype); //true
+console.log(Person.prototype.isPrototypeOf(jonas)); //true
+console.log(Person.prototype.isPrototypeOf(Person)); //false
+
+//we can set property in prototype.those arent directly instead found in prototype
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas); //in prototype..its not own property
+
+console.log(jonas.hasOwnProperty('firstName')); //true
+console.log(jonas.hasOwnProperty('species')); //false.not own.but has access because of prototype
