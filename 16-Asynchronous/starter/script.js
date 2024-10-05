4a89a30f1b2766e362ed12045418385676ed8c02
Farhana Alam Japse <farhanjapz@gmail.com>
2024-10-05 11:16:53 +0600

    fetch and promise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -39,7 +39,7 @@ const getCountry = function (country) {
   });
 };
 getCountry('portugal');
-getCountry('bangladesh');*/
+getCountry('bangladesh');
 
 //callback hell
 const renderCountry = function (data, className = '') {
@@ -103,6 +103,7 @@ const getCountryAndNeighbor = function (country) {
 };
 getCountryAndNeighbor('usa');
 
+//callback hell
 setTimeout(() => {
   console.log('1 sec passed');
   setTimeout(() => {
@@ -114,4 +115,64 @@ setTimeout(() => {
       }, 4000);
     }, 3000);
   }, 2000);
-}, 1000);
+}, 1000);*/
+
+//promises and fetch API
+
+//now using fetch
+const renderCountry = function (data, className = '') {
+  const btn = document.querySelector('.btn-country');
+  const countriesContainer = document.querySelector('.countries');
+
+  const html = `
+            <article class="country ${className}">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)} people</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+              </div>
+            </article>
+      `;
+  //insert this into our page
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       renderCountry(data[0]);
+//     });
+// };
+// getCountryData('portugal');
+
+//two ajax call
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      renderCountry(data[0]);
+
+      const neighbor = data[0].borders?.[0];
+      if (!neighbor) return;
+      return fetch(`https://restcountries.com/v2/alpha/${neighbor}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+getCountryData('singapore');
